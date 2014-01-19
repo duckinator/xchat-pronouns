f4616590aa87db352d8b1fc44047deb83c6b655b
Nik Markwell <nik@duckinator.net>
2014-01-19 16:50:14 -0500

    Massive refactor.

--- xchat-pronouns.py
+++ xchat-pronouns.py
@@ -6,76 +6,76 @@ __module_description__ = "Set information about your personal pronouns, and fetc
 
 pronouns_regexp = " \\[pronouns: (.*)\\]$"
 
+def real_name():
+    return xchat.get_prefs("irc_real_name")
+
 def set_real_name(realname):
-  xchat.command("set irc_real_name " + realname)
+    xchat.command("set irc_real_name " + realname)
 
-def get_pronouns_from_string(real_name):
-  m = re.search(pronouns_regexp, real_name, flags=re.IGNORECASE)
-  if m is None:
-    return None
-  else:
-    return m.group(1)
+def get_pronouns(string):
+    m = re.search(pronouns_regexp, string, flags=re.IGNORECASE)
+    if m is None:
+        return None
+    else:
+        return m.group(1)
 
-def get_pronouns():
-  return get_pronouns_from_string(xchat.get_prefs("irc_real_name"))
-
-def get_real_name_without_pronouns_from_string(irc_real_name):
-  pronouns = get_pronouns_from_string(irc_real_name)
+# Given a string, strip pronouns from it.
+def strip_pronouns(irc_real_name):
+    pronouns = get_pronouns(irc_real_name)
   
-  # The 14 is to account for for " [pronouns: ]".
-  if pronouns:
-    irc_real_name = irc_real_name[0:(len(irc_real_name) - len(pronouns) - 13)]
-  return irc_real_name
+    # The 14 is to account for for " [pronouns: ]".
+    if pronouns:
+        irc_real_name = irc_real_name[0:(len(irc_real_name) - len(pronouns) - 13)]
+    return irc_real_name
 
-def get_real_name_without_pronouns():
-  return get_real_name_without_pronouns_from_string(xchat.get_prefs("irc_real_name"))
+def my_pronouns():
+    return get_pronouns(real_name())
 
 def set_pronouns(new_pronouns):
-  irc_real_name = get_real_name_without_pronouns()
-  
-  pronouns_text = " [pronouns: " + new_pronouns + "]"
-  
-  new_real_name = irc_real_name + pronouns_text
-  
-  if len(new_real_name) > 50:
-    xchat.prnt("The total length for your \"real name\" setting must be at most 50 characters.")
-    xchat.prnt("Setting your preferred pronouns to \"" + new_pronouns + "\" would make it " + str(len(new_real_name)) + " characters.")
-    xchat.prnt("Go to XChat, then Network List, put a shorter value in the \"Real name\" box, and then try again")
-    return False
-  else:
-    set_real_name(irc_real_name + pronouns_text)
-    return True
+    irc_real_name = strip_pronouns(real_name())
+    
+    pronouns_text = " [pronouns: " + new_pronouns + "]"
+    
+    new_real_name = irc_real_name + pronouns_text
+    
+    if len(new_real_name) > 50:
+        xchat.prnt("The total length for your \"real name\" setting must be at most 50 characters.")
+        xchat.prnt("Setting your preferred pronouns to \"" + new_pronouns + "\" would make it " + str(len(new_real_name)) + " characters.")
+        xchat.prnt("Go to XChat, then Network List, put a shorter value in the \"Real name\" box, and then try again")
+        return False
+    else:
+        set_real_name(new_real_name)
+        return True
 
 def get_others_pronouns(handle):
-  return "Not implemented."
+    return "Not implemented."
 
 pronouns_usage = "Usage: PRONOUNS <handle>, get the preferred pronouns for <handle>, if they have it specified in their IRC realname string."
 def cmd_pronouns(word, word_eol, userdata):
-  if len(word) > 0:
-    xchat.prnt("[Not implemented.]")
-  else:
-    xchat.prnt(pronouns_usage)
-  return xchat.EAT_ALL
+    if len(word) > 0:
+        xchat.prnt("[Not implemented.]")
+    else:
+        xchat.prnt(pronouns_usage)
+    return xchat.EAT_ALL
 
 setpronouns_usage = "Usage: SETPRONOUNS <pronouns>, Set your own preferred pronouns. <pronouns> can be whatever text you'd like. Requires reconnecting to take effect.\n                               If <pronouns> is not specified, returns your currently-configured pronouns."
 def cmd_setpronouns(word, word_eol, userdata):
-  if len(word_eol) >= 2:
-    if set_pronouns(word_eol[1]):
-      xchat.prnt("Pronouns set to: " + get_pronouns())
-  else:
-    pronouns = get_pronouns()
-    if pronouns:
-      xchat.prnt("Your pronouns are currently set to: " + pronouns)
+    if len(word_eol) >= 2:
+        if set_pronouns(word_eol[1]):
+            xchat.prnt("Pronouns set to: " + my_pronouns())
     else:
-      xchat.prnt("You haven't set your pronouns yet!")
-      xchat.prnt("If you're not sure how, run /help SETPRONOUNS.")
-  return xchat.EAT_ALL
+        if my_pronouns():
+            xchat.prnt("Your pronouns are currently set to: " + my_pronouns())
+        else:
+            xchat.prnt("You haven't set your pronouns yet!")
+            xchat.prnt("If you're not sure how, run /help SETPRONOUNS.")
+    return xchat.EAT_ALL
 
 unsetpronouns_usage = "Usage: UNSETPRONOUNS, Unset your preferred pronouns."
 def cmd_unsetpronouns(word, word_eol, userdata):
-  set_real_name(get_real_name_without_pronouns())
-  xchat.prnt("Your pronouns have been unset.")
-  return xchat.EAT_ALL
+    set_real_name(strip_pronouns(real_name()))
+    xchat.prnt("Your pronouns have been unset.")
+    return xchat.EAT_ALL
 
 xchat.prnt("Pronouns script initialized")
 
@@ -86,24 +86,31 @@ xchat.hook_command("CLEARPRONOUNS", cmd_unsetpronouns, help=unsetpronouns_usage)
 
 
 
-# WHOIS hooks.
-
-# Standard whois reply:
-# :adams.freenode.net 311 duckinator duckinator ~duck botters/staff/duckinator * :Nik <http://duckyy.net> [pronouns: singular they]
-# Printed as:
-# * [duckinator] (~duck@botters/staff/duckinator): Nik <http://duckyy.net> [pronouns: singular they]
-
 def whois_callback(word, word_eol, userdata):
-  nickname = word[3]
-  username = word[4]
-  host = word[5]
-  raw_real_name = word_eol[7][1:]
-  real_name = get_real_name_without_pronouns_from_string(raw_real_name)
-  pronouns = get_pronouns_from_string(raw_real_name)
-  
-  xchat.emit_print("WhoIs Name Line", nickname, username, host, real_name)
-  xchat.emit_print("WhoIs Identified", nickname, "Preferred gender pronouns: " + pronouns)
-  return xchat.EAT_XCHAT
+    # Response format:
+    # :server 311 yournick othersnick ~othersuser others/host * :Other's Realname
 
-# TODO: Do we need to unhook this?
+    _, _, _, nickname, username, host = word[0:6]
+    raw_real_name = word_eol[7][1:]
+
+    # Get the real name without preferred pronouns. If they don't have their
+    # preferred pronouns specified in their WHOIS information, it simply shows
+    # their real name string as-is.
+    real_name = strip_pronouns(raw_real_name)
+
+    # Get their preferred pronouns, or None.
+    pronouns = get_pronouns(raw_real_name)
+
+    # Print the first line of the WHOIS response, sans pronouns snippet at the end,
+    # exactly how XChat would print it otherwise.
+    xchat.emit_print("WhoIs Name Line", nickname, username, host, real_name)
+
+    # Print their preferred pronouns as the second line of the WHOIS information,
+    # if they specified anything. Otherwise, do nothing.
+    if pronouns is not None:
+        xchat.emit_print("WhoIs Identified", nickname, "Preferred pronouns: " + pronouns)
+
+    # Stop XChat from continuing with its default behaviors.
+    return xchat.EAT_XCHAT
+
 xchat.hook_server("311", whois_callback)
