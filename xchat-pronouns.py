12093abeb89e06e9e3e43190428e8fdad4e1bfcf
Nik Markwell <nik@duckinator.net>
2014-01-19 03:05:14 -0500

    Fix up /SETPRONOUNS and /UNSETPRONOUNS (aka /CLEARPRONOUNS) a bit; add /WHOIS hooks.

--- xchat-pronouns.py
+++ xchat-pronouns.py
@@ -9,26 +9,29 @@ pronouns_regexp = " \\[pronouns: (.*)\\]$"
 def set_real_name(realname):
   xchat.command("set irc_real_name " + realname)
 
-def get_pronouns():
-  irc_real_name = xchat.get_prefs("irc_real_name")
-  m = re.search(pronouns_regexp, irc_real_name, flags=re.IGNORECASE)
+def get_pronouns_from_string(real_name):
+  m = re.search(pronouns_regexp, real_name, flags=re.IGNORECASE)
   if m is None:
     return None
   else:
     return m.group(1)
 
-def get_realname_without_pronouns():
-  irc_real_name = xchat.get_prefs("irc_real_name")
-  pronouns = get_pronouns()
+def get_pronouns():
+  return get_pronouns_from_string(xchat.get_prefs("irc_real_name"))
+
+def get_real_name_without_pronouns_from_string(irc_real_name):
+  pronouns = get_pronouns_from_string(irc_real_name)
   
   # The 14 is to account for for " [pronouns: ]".
   if pronouns:
     irc_real_name = irc_real_name[0:(len(irc_real_name) - len(pronouns) - 13)]
-  
   return irc_real_name
 
+def get_real_name_without_pronouns():
+  return get_real_name_without_pronouns_from_string(xchat.get_prefs("irc_real_name"))
+
 def set_pronouns(new_pronouns):
-  irc_real_name = get_realname_without_pronouns()
+  irc_real_name = get_real_name_without_pronouns()
   
   pronouns_text = " [pronouns: " + new_pronouns + "]"
   
@@ -70,7 +73,7 @@ def cmd_setpronouns(word, word_eol, userdata):
 
 unsetpronouns_usage = "Usage: UNSETPRONOUNS, Unset your preferred pronouns."
 def cmd_unsetpronouns(word, word_eol, userdata):
-  set_real_name(get_realname_without_pronouns())
+  set_real_name(get_real_name_without_pronouns())
   xchat.prnt("Your pronouns have been unset.")
   return xchat.EAT_ALL
 
@@ -80,3 +83,27 @@ xchat.hook_command("PRONOUNS", cmd_pronouns, help=pronouns_usage)
 xchat.hook_command("SETPRONOUNS", cmd_setpronouns, help=setpronouns_usage)
 xchat.hook_command("UNSETPRONOUNS", cmd_unsetpronouns, help=unsetpronouns_usage)
 xchat.hook_command("CLEARPRONOUNS", cmd_unsetpronouns, help=unsetpronouns_usage)
+
+
+
+# WHOIS hooks.
+
+# Standard whois reply:
+# :adams.freenode.net 311 duckinator duckinator ~duck botters/staff/duckinator * :Nik <http://duckyy.net> [pronouns: singular they]
+# Printed as:
+# * [duckinator] (~duck@botters/staff/duckinator): Nik <http://duckyy.net> [pronouns: singular they]
+
+def whois_callback(word, word_eol, userdata):
+  nickname = word[3]
+  username = word[4]
+  host = word[5]
+  raw_real_name = word_eol[7][1:]
+  real_name = get_real_name_without_pronouns_from_string(raw_real_name)
+  pronouns = get_pronouns_from_string(raw_real_name)
+  
+  xchat.emit_print("WhoIs Name Line", nickname, username, host, real_name)
+  xchat.emit_print("WhoIs Identified", nickname, "Preferred gender pronouns: " + pronouns)
+  return xchat.EAT_XCHAT
+
+# TODO: Do we need to unhook this?
+xchat.hook_server("311", whois_callback)
