f0b2c92c54192f4c3175c075fabc9b5e001d608f
Marie Markwell <me@marie.so>
2015-12-08 13:23:04 -0500

    lol

--- README.markdown
+++ README.markdown
@@ -1,22 +1,22 @@
-# Preferred pronouns script
+# Personal pronouns script
 
 * `/PRONOUNS <handle>` (not yet implemented)
-    <p>Get the preferred pronouns for `<handle>`, if they have it specified in their IRC realname string.</p>
+    <p>Get the pronouns for `<handle>`, if they have it specified in their IRC realname string.</p>
 * `/SETPRONOUNS <pronouns>`
-    <p>Set your own preferred pronouns. `<pronouns>` can be whatever text you'd like. Requires reconnecting to take effect.</p>
+    <p>Set your own pronouns. `<pronouns>` can be whatever text you'd like. Requires reconnecting to take effect.</p>
     <p>If you don't pass any arguments it returns your currently-configured pronouns.</p>
 * `/UNSETPRONOUNS` (alias: `/CLEARPRONOUNS`)
-    <p>Clear any preferred pronouns you have specified.</p>
+    <p>Clear any pronouns you have specified.</p>
 
 ## WHOIS
 
-`xchat-pronouns` also ties into WHOIS replies, and adds a line starting with "Preferred pronouns:" for anyone who specifies them.
+`xchat-pronouns` also ties into WHOIS replies, and adds a line starting with "Pronouns:" for anyone who specifies them.
 
 Example:
 
 ```
-* [duckinator] (~duck@botters/staff/duckinator): Marie Markwell
-* [duckinator] Preferred pronouns: she or they
+* [duckinator] (~duckie@unaffiliated/duckinator): Marie Markwell
+* [duckinator] Pronouns: they/them/themself
 * [duckinator] hitchcock.freenode.net :Sofia, BG, EU
 * duckinator :is using a secure connection
 * [duckinator] is logged in as duckyy
@@ -33,8 +33,8 @@ Reconnect.
 
     /whois foobar
 
-The WHOIS response for yourself should include your preferred pronouns as you entered them.
+The WHOIS response for yourself should include your pronouns as you entered them.
 
 ## Notes
 
-Due to how this is implemented (using realname field), you have to reconnect (or restart XChat) for changes to your preferred pronouns to take effect.
+Due to how this is implemented (using realname field), you have to reconnect (or restart XChat) for changes to your pronouns to take effect.
--- xchat-pronouns.py
+++ xchat-pronouns.py
@@ -12,7 +12,7 @@ def real_name():
 def set_real_name(new_real_name):
     xchat.command("set irc_real_name " + new_real_name)
 
-# Given a string, return the preferred pronouns from it.
+# Given a string, return the pronouns from it.
 def get_pronouns(string):
     m = re.search(pronouns_regexp, string, flags=re.IGNORECASE)
     if m is None:
@@ -23,7 +23,7 @@ def get_pronouns(string):
 # Given a string, strip pronouns from it.
 def strip_pronouns(string):
     pronouns = get_pronouns(string)
-    
+
     # Take everything before the pronouns, if any are in the string.
     if pronouns:
         string = string[0:(len(string) - len(pronouns) - 13)]
@@ -36,14 +36,14 @@ def my_pronouns():
 # Set pronouns to the specified string.
 def set_pronouns(new_pronouns):
     base_real_name = strip_pronouns(real_name())
-    
+
     pronouns_text = " [pronouns: " + new_pronouns + "]"
-    
+
     new_real_name = base_real_name + pronouns_text
-    
+
     if len(new_real_name) > 50:
         xchat.prnt("The total length for your \"real name\" setting must be at most 50 characters.")
-        xchat.prnt("Setting your preferred pronouns to \"" + new_pronouns + "\" would make it " + str(len(new_real_name)) + " characters.")
+        xchat.prnt("Setting your pronouns to \"" + new_pronouns + "\" would make it " + str(len(new_real_name)) + " characters.")
         xchat.prnt("Go to XChat, then Network List, put a shorter value in the \"Real name\" box, and then try again")
         return False
     else:
@@ -53,7 +53,7 @@ def set_pronouns(new_pronouns):
 def get_others_pronouns(handle):
     return "Not implemented."
 
-pronouns_usage = "Usage: PRONOUNS <handle>, get the preferred pronouns for <handle>, if they have it specified in their IRC realname string."
+pronouns_usage = "Usage: PRONOUNS <handle>, get the pronouns for <handle>, if they have it specified in their IRC realname string."
 def cmd_pronouns(word, word_eol, userdata):
     if len(word) > 0:
         xchat.prnt("[Not implemented.]")
@@ -61,7 +61,7 @@ def cmd_pronouns(word, word_eol, userdata):
         xchat.prnt(pronouns_usage)
     return xchat.EAT_ALL
 
-setpronouns_usage = "Usage: SETPRONOUNS <pronouns>, Set your own preferred pronouns. <pronouns> can be whatever text you'd like. Requires reconnecting to take effect.\nIf <pronouns> is not specified, returns your currently-configured pronouns."
+setpronouns_usage = "Usage: SETPRONOUNS <pronouns>, Set your own pronouns. <pronouns> can be whatever text you'd like. Requires reconnecting to take effect.\nIf <pronouns> is not specified, returns your currently-configured pronouns."
 def cmd_setpronouns(word, word_eol, userdata):
     if len(word_eol) >= 2:
         if set_pronouns(word_eol[1]):
@@ -74,7 +74,7 @@ def cmd_setpronouns(word, word_eol, userdata):
             xchat.prnt("If you're not sure how, run /help SETPRONOUNS.")
     return xchat.EAT_ALL
 
-unsetpronouns_usage = "Usage: UNSETPRONOUNS, Unset your preferred pronouns."
+unsetpronouns_usage = "Usage: UNSETPRONOUNS, Unset your pronouns."
 def cmd_unsetpronouns(word, word_eol, userdata):
     set_real_name(strip_pronouns(real_name()))
     xchat.prnt("Your pronouns have been unset.")
@@ -96,22 +96,22 @@ def whois_callback(word, word_eol, userdata):
     _, _, _, nickname, username, host = word[0:6]
     raw_real_name = word_eol[7][1:]
 
-    # Get the real name without preferred pronouns. If they don't have their
-    # preferred pronouns specified in their WHOIS information, it simply shows
-    # their real name string as-is.
+    # Get the real name without pronouns. If they don't have their pronouns
+    # specified in their WHOIS information, it simply shows their real name
+    # string as-is.
     real_name = strip_pronouns(raw_real_name)
 
-    # Get their preferred pronouns, or None.
+    # Get their pronouns, or None.
     pronouns = get_pronouns(raw_real_name)
 
     # Print the first line of the WHOIS response, sans pronouns snippet at the end,
     # exactly how XChat would print it otherwise.
     xchat.emit_print("WhoIs Name Line", nickname, username, host, real_name)
 
-    # Print their preferred pronouns as the second line of the WHOIS information,
+    # Print their pronouns as the second line of the WHOIS information,
     # if they specified anything. Otherwise, do nothing.
     if pronouns is not None:
-        xchat.emit_print("WhoIs Identified", nickname, "Preferred pronouns: " + pronouns)
+        xchat.emit_print("WhoIs Identified", nickname, "Pronouns: " + pronouns)
 
     # Stop XChat from printing the default first WHOIS line.
     return xchat.EAT_XCHAT
